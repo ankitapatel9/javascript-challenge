311846c1ea1362824782802717a8c3e86fa671f6
Anki Patel <batuk.girl91@gmail.com>
2019-11-25 13:53:25 -0600

    added javascript file

---
+++ static/js/app.js
@@ -0,0 +1,33 @@
+// from data.js
+var tableData = data;
+var tbody = d3.select("tbody");
+function newTableData(data){
+    
+    data.forEach((ufoData) => {
+        console.log(ufoData);
+        var row = tbody.append("tr");
+        Object.entries(ufoData).forEach(([key, value]) => {
+        console.log(key, value);
+        //   Append a cell to the row for each value
+        // in the weather report object
+        var cell = row.append("td");
+        cell.text(value);
+        });
+    });
+}
+newTableData(tableData);
+
+// YOUR CODE HERE!
+var inputField = d3.select("#datetime");
+var button = d3.selectAll("filter-btn");
+function eventHandler(){
+    d3.event.preventDefault();
+    tbody.text("");
+    var filteredTable = tableData.filter(data => data.datetime === inputField.property("value"));
+    console.log(filteredTable);
+    newTableData(filteredTable);
+}
+  
+  // Input fields can trigger a change event when new text is entered.
+button.on("click", eventHandler);
+inputField.on("change", eventHandler);
